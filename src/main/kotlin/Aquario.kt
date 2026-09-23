547018413203211c0d72b73399e865569e3a00fa
anxovaz <avazquezlorenzo@danielcastelao.org>
2026-09-23 13:00:12 +0200

    Agrego Aquario clase

---
+++ src/main/kotlin/Aquario.kt
@@ -0,0 +1,24 @@
+class Aquario {
+    var ancho: Int = 0
+    var largo: Int = 0
+    var litrosLlenados: Int = 0
+    val encargadoMantenimiento: String = "Pepe"
+
+    constructor(ancho: Int = 100, largo: Int = 40, litrosLlenados: Int = 3000) {
+        this.ancho = ancho
+        this.largo = largo
+        this.litrosLlenados = litrosLlenados
+    }
+
+    fun mostrar(): String {
+        return "Llenados: $litrosLlenados litros \nAncho: $ancho \nLargo: $largo \nEncargado mantenimiento: $encargadoMantenimiento"
+    }
+
+}
+fun main() {
+    val aquario = Aquario(largo = 50)
+    println(aquario.mostrar())
+    println("---")
+    aquario.litrosLlenados = 2000
+    println(aquario.mostrar())
+}
